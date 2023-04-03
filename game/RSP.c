6212d525db4087fb299657e906783139a1dfed6e
bellday <92672351+bellday@users.noreply.github.com>
2023-04-03 14:26:10 +0900

    minigame
    1 : 가위바위보 2: 객관식 문제 3: 구구단 4:숫자야구 5:단어맞추기 6:업앤다운

--- game/RSP.c
+++ game/RSP.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include  <stdlib.h>// rand 함수 사용
 #include <time.h> // 시간을 통해 랜덤 사용
@@ -273,13 +274,239 @@ while (1)
 
 
 
-      
 
+int number_baseball() 
+
+{ 
+
+  int computerBall[3]; // 컴퓨터의 볼을 저장할 int형 배열 
+
+  int i, j; // 반복을 위한 변수 
+
+int temp; // 난수 중복 체크를 위한 변수 
+
+int userBall[3]; // 사용자의 볼을 저장할 int형 배열 
+
+  int count = 1; // 회차를 확인할 변수 9회까지 가능 
+
+  int strike; // 스트라이크의 수를 세기 위한 변수 
+
+  int ball; // 볼의 수를 세기 위한 변수 
+
+  // 난수 생성 및 중복 체크 
+
+  srand((unsigned)time(NULL)); 
+
+  for (i = 0; i < 3; i++) 
+
+  { 
+
+      temp = (rand() % 9) + 1; // temp변수에 현재 랜덤 값을 저장 
+
+computerBall[i] = temp; // temp값을 컴퓨터의 볼에 저장 
+
+      for (j = 0; j < i; j++) // 현재 추가된 만큼 반복하며 
+
+          if (temp == computerBall[j] && i != j) // 만약 현재 temp값과 이전에 넣은 값 중 동일한 게 있다면 
+
+              i--; // i의 값을 줄여 다시 이전으로 돌아가도록 
+
+  } 
+
+   
+
+  while (1) // 숫자야구 게임 시작 
+
+  { 
+
+      printf("[%d회차 숫자야구]\n", count); 
+
+while(1) // 정상적인 숫자만 입력받기 위한 반복 
+
+{ 
+
+printf("1부터 9까지의 숫자를 한번씩 입력하세요 : \n"); 
+
+scanf("%d %d %d", &userBall[0], &userBall[1], &userBall[2]); 
+
+if(userBall[0] < 1 || userBall[0] > 9 || userBall[1] < 1 || userBall[1] > 9 || userBall[2] < 1 || userBall[2] > 9) 
+
+{ // 입력한 숫자가 1 ~ 9 숫자가 아니면 다시 입력받도록 
+
+printf("범위 외의 숫자를 입력하시면 안됩니다.\n"); 
+
+continue; 
+
+} 
+
+else if(userBall[0] == userBall[1] || userBall[0] == userBall[2] || userBall[1] == userBall[2]) 
+
+{ // 입력한 숫자 중에 중복된 게 있으면 다시 입력받도록 
+
+printf("중복된 숫자를 입력하시면 안됩니다.\n"); 
+
+continue; 
+
+} 
+
+break; // 아무 문제 없을 경우 반복 종료 
+
+} 
+
+      ball = 0; 
+
+      strike = 0; 
+
+      for (i = 0; i < 3; i++) // computerBall 1~3번째에 따라 각 userBall 1~3번째를 확인하기 위한 반복 
+
+          for (j = 0; j < 3; j++) 
+
+              if (computerBall[i] == userBall[j]) // computerBall과 userBall의 값이 같다면 
+
+                  if (i == j) // 같은 위치일 경우 
+
+                      strike++; // 스트라이크 개수 증가 
+
+                  else // 다른 위치일 경우 
+
+                      ball++; // 볼 개수 증가 
+
+// 확인한 결과를 출력 
+
+      printf("[결과]스트라이크 : %d, 볼 : %d\n\n", strike, ball); 
+
+if(strike == 3) // 만약 스트라이크 개수가 3개이면 승리 
+
+{ 
+
+printf("***** 승리했습니다. *****\n\n"); 
+
+break; 
+
+} 
+
+else if(count == 9) // 만약 9회차인데도 승리하지 못하면 패배 
+
+{ 
+
+printf("***** 패배했습니다.. 정답 : %d %d %d *****\n\n", computerBall[0], computerBall[1], computerBall[2]); 
+
+break; 
+
+} 
+
+      count++; 
+
+  } 
+
+return 0; 
+
+} 
+
+
+int hangman() {
+    srand(time(NULL));
+ 
+    char word[5][7]= { "grapes", "citron", "cherry", "orange", "banana" };
+    
+    char* answer = word[rand() % 5];
+    char blind[]= "______";
+    char input;
+    int try; //횟수
+    int key;
+
+    int j; // j==글자수
+    int chance=12;
+ 
+
+    while (1) {
+        try = 0;
+        key = 0;
+ 
+        printf("%s : \n", blind);
+        printf("Hint: 과일\n");
+        scanf(" %c", &input);
+
+        // 입력 단어가 정답 문장에 있는지 확인
+        for (int i = 0; i < 6; i++) {
+            if (input == blind[i]) {
+                key = 1;
+                printf("이미 등록한 문자입니다!! 재입력하세요!! \n");
+                break;
+            }
+            else if (input == answer[i]) {
+                blind[i] = input;
+                try++;
+            }
+        }
+ 
+        // 이미 등록한 문자라면 재입력 받아주기
+        if (key == 1) {
+            continue;
+        }
+ 
+        // 하나도 없으면 목숨 1개 잃음. 3개 잃으면 게임 오버
+        if (try == 0) {
+            chance--;
+            printf("기회가 %d번 남았습니다\n",chance);
+            if (chance == 0) {
+                printf("실패\n");
+                break;
+            }
+        }
+ 
+        // 모든 단어를 다 맞췄다면 정답을 출력하고 종료
+        if (strcmp(blind, answer) == 0) {
+            printf("%s\n", blind);
+            printf("정답");
+            break;
+        }
+    }
+ 
+}
+      
+int upanddown() {
+	int rand_num =0; //랜덤값저장
+	int input;
+    int chance=10;
+
+	srand((unsigned int)time(NULL));
+	rand_num= rand() % 100+1;   //0~100
+
+	while (1) {
+        if(chance==0){
+            printf("실패\n");
+            break;
+        }
+		printf("숫자를 입력하세요(0~100)\n남은 기회 %d회\n", chance);
+		scanf("%d", &input);
+        
+		if (input < 0 || input > 100) {
+			printf("범위에 벗어난 숫자 입니다\n");
+			//continue;
+        }
+		else if (input == rand_num) {
+			printf("정답입니다.\n");
+			break;
+		}
+		else if (input > rand_num) {
+			printf("DOWN\n");
+             chance--;
+		}
+		else if (input < rand_num && input > 0 && input < 100){
+			printf("UP\n");
+             chance--;     
+        }
+        else
+        {printf("숫자가 아닙니다");}
+    }
+
+}
 
 int main(){
     int number;
     printf("게임을 선택하세요! \n");
-    printf("1 : 가위바위보 2: 객관식 문제 3: 구구단\n");
+    printf("1 : 가위바위보 2: 객관식 문제 3: 구구단 4:숫자야구 5:단어맞추기 6:업앤다운\n");
     scanf("%d",&number);
     if (number==1){
     RSP();    
@@ -291,7 +518,18 @@ int main(){
     else if (number ==3) 
     {
             Multiplication();
-      }
-
+    }
+    else if (number ==4) 
+    {
+            number_baseball();
+    }
+    else if (number ==5) 
+    {
+            hangman();
+    }
+    else if (number ==6) 
+    {
+            upanddown();
+    }
 }
 
